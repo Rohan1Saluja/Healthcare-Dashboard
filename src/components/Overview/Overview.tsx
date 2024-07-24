29dfe539432658111bcd01181700374192742102
Rohan Saluja <rohansalujamusic@gmail.com>
2024-07-24 13:08:44 +0530

    Update Overview.tsx
    minor ui fixes

--- src/components/Overview/Overview.tsx
+++ src/components/Overview/Overview.tsx
@@ -27,13 +27,13 @@ export const Overview: React.FC = () => {
             onClick={handleNavClick}
           />
         </span>
-      </div>
       <div className="page-description">
         <Text text="The scope of this project was to design and populate the patients' tab of the Healthcare Dashboard." className="description"/>
         <span> 
           <Text text="If you're a developer and wish to contribute to this project, feel free to check out the " className="description-mid"/>
           <Text text="GitHub Repo" className="description-mid accent link" onClick={handleGitHubClick}/>
         </span>
+      </div>
       </div>
     </div>
   );
